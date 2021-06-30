e622adf282b35543b375b284b8a3e87b22bf5c7a
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-29 23:28:42 -0300

    add has section conditional

--- javascriptes6/js/main.js
+++ javascriptes6/js/main.js
@@ -1,20 +1,17 @@
-(function () {
+(function (document, window) {
+  const $tabMenu = document.querySelectorAll('[data-js="tab-menu"] li');
+  const $tabContent = document.querySelectorAll('[data-js="tab-content"] section');
 
+  if ($tabMenu.length && $tabContent.length) {
+    $tabContent[0].classList.add('ativo');
 
+    function activeTab(index) {
+      $tabContent.forEach(section => section.classList.remove('ativo'));
+      $tabContent[index].classList.add('ativo');
+    }
 
-
-
-})();
-
-const $tabMenu = document.querySelectorAll('[data-js="tab-menu"] li');
-const $tabContent = document.querySelectorAll('[data-js="tab-content"] section');
-$tabContent[0].classList.add('ativo');
-
-function activeTab(index) {
-  $tabContent.forEach(section => section.classList.remove('ativo'));
-  $tabContent[index].classList.add('ativo');
-}
-
-$tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => activeTab(index))
-})
+    $tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => activeTab(index))
+    })
+  }
+})(document, window);
