03fd60985d125f000ea47a0b4e1d7b7ca0a3aa6b
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-06-30 21:39:54 -0300

    add scroll view function

--- javascriptes6/js/main.js
+++ javascriptes6/js/main.js
@@ -30,4 +30,24 @@
       item.addEventListener('click', faqListAccordion)
     });
   }
+
+  const $menuLinks = document.querySelectorAll('[data-js="menu-link"] a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+
+  $menuLinks.forEach(item => {
+    item.addEventListener('click', scrollToSection)
+  })
+
+
+
 })(document, window);
