fda625bf0f276cdfd346e15e306dd0d8a24bd839
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-09 00:32:59 -0300

    add modal module

--- javascriptes6/js/modules/modal.js
+++ javascriptes6/js/modules/modal.js
@@ -1,20 +1,22 @@
-const $btnAbrir = document.querySelector('[data-js="abrir"]');
-const $btnFechar = document.querySelector('[data-js="modal"]');
-const $containerModal = document.querySelector('[data-js="container"]');
-const activeClass = 'ativo';
+export default function initModal() {
+  const $btnAbrir = document.querySelector('[data-js="abrir"]');
+  const $btnFechar = document.querySelector('[data-js="modal"]');
+  const $containerModal = document.querySelector('[data-js="container"]');
+  const activeClass = 'ativo';
 
-if ($btnAbrir && $btnFechar && $containerModal) {
-  function toggleModal(event) {
-    event.preventDefault();
-    $containerModal.classList.toggle(activeClass);
+  if ($btnAbrir && $btnFechar && $containerModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      $containerModal.classList.toggle(activeClass);
+    }
+
+    function clickForaModal(event) {
+      if (event.target === this)
+        toggleModal(event);
+    }
+
+    $btnAbrir.addEventListener('click', toggleModal)
+    $btnFechar.addEventListener('click', toggleModal)
+    $containerModal.addEventListener('click', clickForaModal)
   }
-
-  function clickForaModal(event) {
-    if (event.target === this)
-      toggleModal(event);
-  }
-
-  $btnAbrir.addEventListener('click', toggleModal)
-  $btnFechar.addEventListener('click', toggleModal)
-  $containerModal.addEventListener('click', clickForaModal)
 }
