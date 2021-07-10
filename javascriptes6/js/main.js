4f32087be336da9fec90ad71cb66e5d2a66a1722
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-10 17:53:16 -0300

    import tooltip function

--- javascriptes6/js/main.js
+++ javascriptes6/js/main.js
@@ -3,9 +3,11 @@ import initScrollAnimacao from './modules/scroll-animacao.js'
 import initModal from './modules/modal.js'
 import initFaqAccordion from './modules/faq-accordion.js'
 import initTabNav from './modules/tab-nav.js'
+import initTooltip from './modules/tooltip.js'
 
 initScrollSuave();
 initScrollAnimacao();
 initModal();
 initFaqAccordion();
 initTabNav();
+initTooltip();
