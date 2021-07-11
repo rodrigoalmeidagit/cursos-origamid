80c9197159b666b7f2d4d5c618e07a9c1d694416
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-11 02:09:03 -0300

    add dropdown menu function

--- javascriptes6/js/modules/dropdown.js
+++ javascriptes6/js/modules/dropdown.js
@@ -1,3 +1,21 @@
-export default function initDropDownMenu() {
+import outSideClick from "./outsideclick.js";
 
+export default function initDropDownMenu() {
+  const $dropdownMenu = document.querySelectorAll('[data-js="dropdown"]');
+  const userEvents = ['touchstart', 'click'];
+
+  $dropdownMenu.forEach((menu) => {
+    userEvents.forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick)
+    })
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+    outSideClick(this, userEvents, () => {
+      this.classList.remove('active');
+    });
+  }
 }
+
