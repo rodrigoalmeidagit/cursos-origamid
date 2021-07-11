abfe625462872593f0a5216e6ee35570065df7fc
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-11 00:15:35 -0300

    add dropdown menu import

--- javascriptes6/js/main.js
+++ javascriptes6/js/main.js
@@ -1,9 +1,10 @@
-import initScrollSuave from './modules/scroll-suave.js'
-import initScrollAnimacao from './modules/scroll-animacao.js'
-import initModal from './modules/modal.js'
-import initFaqAccordion from './modules/faq-accordion.js'
-import initTabNav from './modules/tab-nav.js'
-import initTooltip from './modules/tooltip.js'
+import initScrollSuave from './modules/scroll-suave.js';
+import initScrollAnimacao from './modules/scroll-animacao.js';
+import initModal from './modules/modal.js';
+import initFaqAccordion from './modules/faq-accordion.js';
+import initTabNav from './modules/tab-nav.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown';
 
 initScrollSuave();
 initScrollAnimacao();
@@ -11,3 +12,4 @@ initModal();
 initFaqAccordion();
 initTabNav();
 initTooltip();
+initDropDownMenu();
