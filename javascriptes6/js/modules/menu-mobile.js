9c3d11be207434d893578cd80a0ebb0508869c41
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-14 15:17:57 -0300

    add initmenumobile function

--- javascriptes6/js/modules/menu-mobile.js
+++ javascriptes6/js/modules/menu-mobile.js
@@ -1,3 +1,20 @@
+import outSideClick from './outsideclick.js';
+
 export default function initMenuMobile() {
 
 }
+
+const $menuBtn = document.querySelector('[data-js="menu"]')
+const $menuList = document.querySelector('[data-js="list"]')
+
+function openMenu(event) {
+  $menuList.classList.add('active')
+  $menuBtn.classList.add('active')
+  outSideClick($menuList, ['click', 'touchstart'], () => {
+    console.log('teste')
+    $menuList.classList.remove('active')
+    $menuBtn.classList.remove('active')
+  })
+}
+
+$menuBtn.addEventListener('click', openMenu);
