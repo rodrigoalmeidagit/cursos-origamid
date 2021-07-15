bb145a03148c062db11c90441f308cf29cc66d2b
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-14 23:23:21 -0300

    add if has button conditional

--- javascriptes6/js/modules/menu-mobile.js
+++ javascriptes6/js/modules/menu-mobile.js
@@ -1,20 +1,20 @@
 import outSideClick from './outsideclick.js';
 
 export default function initMenuMobile() {
+  const $menuBtn = document.querySelector('[data-js="menu"]')
+  const $menuList = document.querySelector('[data-js="list"]')
+  const eventos = ['click', 'touchstart'];
 
+  if ($menuBtn) {
+    function openMenu(event) {
+      $menuList.classList.add('active')
+      $menuBtn.classList.add('active')
+      outSideClick($menuList, eventos, () => {
+        console.log('teste')
+        $menuList.classList.remove('active')
+        $menuBtn.classList.remove('active')
+      })
+    }
+    eventos.forEach(evento => $menuBtn.addEventListener(evento, openMenu))
+  };
 }
-
-const $menuBtn = document.querySelector('[data-js="menu"]')
-const $menuList = document.querySelector('[data-js="list"]')
-
-function openMenu(event) {
-  $menuList.classList.add('active')
-  $menuBtn.classList.add('active')
-  outSideClick($menuList, ['click', 'touchstart'], () => {
-    console.log('teste')
-    $menuList.classList.remove('active')
-    $menuBtn.classList.remove('active')
-  })
-}
-
-$menuBtn.addEventListener('click', openMenu);
